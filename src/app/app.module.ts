459069b25c9b766ded291677b72f5debb5c575a9
vishnu <vishnu@NTP-LAP-767.chn.nestgroup.net>
2022-12-07 00:03:52 +0530

    addpatient page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddpatientComponent } from './addpatient/addpatient.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddpatientComponent
   ],
   imports: [
     BrowserModule,
